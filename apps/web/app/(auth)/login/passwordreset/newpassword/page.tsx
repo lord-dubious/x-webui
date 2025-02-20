49ac8a90540446b0577745dc2258739f608ae6d1
Avish Madaan <theavishmadaan@gmail.com>
2025-02-20 20:53:36 +0530

    fixed pass reset

--- apps/web/app/(auth)/login/passwordreset/newpassword/page.tsx
+++ apps/web/app/(auth)/login/passwordreset/newpassword/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { Suspense } from 'react'
-import PasswordReset from './passwordReset'
+import PasswordReset from './PasswordReset'
 
 const PasswordResetPage = () => {
   return (
