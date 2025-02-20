315fb051b54c24a0cb1ac2355d05e5763e69d02f
Avish Madaan <theavishmadaan@gmail.com>
2025-02-20 15:18:18 +0530

    base structure

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,1 @@
+import primsa from "@repo/db/client"
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import { Card } from "@repo/ui/card";
 
-
-
 export default function Page() {
   return (
    <div className="text-yellow-200 bg-white">
