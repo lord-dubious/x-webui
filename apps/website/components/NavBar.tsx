de3967d01ff6fd77fd2f1f49aad0326404a8442b
Avish Madaan <theavishmadaan@gmail.com>
2025-02-22 12:24:13 +0530

    basic changes

--- apps/website/components/NavBar.tsx
+++ apps/website/components/NavBar.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="bg-black text-white">
+    <nav className="bg-black text-white border-b border-opacity-20 border-gray-50">
       <div className="max-w-7xl mx-auto px-6 py-5 flex items-center justify-between">
-        <div className="flex items-center space-x-3">
-          <img src="/logo.jpg" alt="Tweetly Logo" className="h-10 w-auto" />
-          <span className="text-2xl font-bold">Tweetly</span>
+        <div className="flex items-center ">
+          <img src="/logo.jpg" alt="Tweetly Logo" className="h-12 w-auto" />
+          <span className="text-3xl font-bold">Tweetly</span>
         </div>
         <div className="hidden md:flex items-center space-x-8">
           <Link href="/" className="hover:text-blue-400 transition">
